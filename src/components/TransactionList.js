270a27faaed919112e9d7adaa2a4f8a28b6f69d3
Sudhanshu Vishwakarma <96513467+Sudhanshu10m@users.noreply.github.com>
2023-04-15 23:42:43 +0530

    Completed the frontend

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  // console.log(context);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} /> 
+          // Note that Strings as an ID woould result in identical ID so better use some variable and note a string in this case:
+          // {Transaction.id} instead of "Transaction.id"
+        ))}
       </ul>
     </>
   );
